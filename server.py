7d20595a562e34c327a38ca346499fc125c5d305
TheGarten <williambizier21@gmail.com>
2024-11-16 22:30:04 -0500

    start page

--- server.py
+++ server.py
@@ -26,56 +26,52 @@ def index():
     return render_template('pages/index.html')
 
 
-# @app.route('/login', methods=['POST', 'GET'])
-# def login():
-#     # check if users logged in, if yes redirect to feed
-#     token = request.cookies.get('auth', None)
-#     if (token != None and database.check_token(token)):
-#         response = make_response(redirect(url_for('index', _external=True)))
-#         response.set_cookie('auth', token)
-#         return response
-#     # if the user is requesting the login form
-#     if request.method == 'GET':
-#         response = make_response(render_template('login.html', error=''), 200)
-#         response.headers['X-Content-Type-Options'] = 'nosniff'
-#         return response
-#     # submitting login form
-#     else:
-#         username = request.form.get('username', None)
-#         if username != None and username != '' and username != ' ':
-#             username = html.escape(username)
-#         else:
-#             response = make_response(render_template(
-#                 'login.html', error='Username Empty'), 200)
-#             response.headers['X-Content-Type-Options'] = 'nosniff'
-#             return response
-#         password = request.form.get('password')
-#         if (password == None or password == ''):
-#             response = make_response(render_template(
-#                 'login.html', error='password empty'), 200)
-#             response.headers['X-Content-Type-Options'] = 'nosniff'
-#             return response
-#         user = database.get_user_by_username(username)
-#         if (user == None):
-#             response = make_response(render_template(
-#                 'login.html', error='No User Found'), 200)
-#             response.headers['X-Content-Type-Options'] = 'nosniff'
-#             return response
-#         else:
-#             # Login has all info needed just need to check password hash
-#             ph = bcrypt.hashpw(password.encode(), user.salt)
-#             if ph == user.passhash:
-#                 token = secrets.token_hex()
-#                 database.set_user_token(
-#                     username, token, datetime.datetime.now(tz=datetime.timezone.utc))
-#                 response = make_response(
-#                     redirect(url_for('index', _external=True)))
-#                 response.set_cookie('auth', token)
-#                 return response
-#             response = make_response(render_template(
-#                 'login.html', error='Password Incorrect', login=login), 200)
-#             response.headers['X-Content-Type-Options'] = 'nosniff'
-#             return response
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    # Check if user is already logged in
+    token = request.cookies.get('auth', None)
+    if token is not None and database.check_token(token):
+        response = make_response(redirect(url_for('index', _external=True)))
+        response.set_cookie('auth', token)
+        return response
+
+    # Handle login form submission
+    if request.method == 'POST':
+        username = request.form.get('username', '').strip()
+        password = request.form.get('password', '').strip()
+
+        if not username:
+            error = 'Username Empty'
+        elif not password:
+            error = 'Password Empty'
+        else:
+            username = html.escape(username)
+            user = database.get_user_by_username(username)
+            if user is None:
+                error = 'No User Found'
+            else:
+                # Verify password
+                ph = bcrypt.hashpw(password.encode(), user.salt)
+                if ph == user.passhash:
+                    token = secrets.token_hex()
+                    database.set_user_token(
+                        username, token, datetime.datetime.now(tz=datetime.timezone.utc)
+                    )
+                    response = make_response(redirect(url_for('index', _external=True)))
+                    response.set_cookie('auth', token)
+                    return response
+                else:
+                    error = 'Password Incorrect'
+        
+        # If there's an error, render the index page with the error message
+        response = make_response(render_template('index.html', error=error))
+        response.headers['X-Content-Type-Options'] = 'nosniff'
+        return response
+
+    # For GET requests, render the index page with no error
+    response = make_response(render_template('index.html', error=''))
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    return response
 
 
 app.run(debug=True, host='127.0.0.1', port=8080)
