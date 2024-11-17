b369de12592fc7cdf1bc2699d5c90bdc36f962f7
TheGarten <williambizier21@gmail.com>
2024-11-17 04:44:35 -0500

    login errors fixed

--- server.py
+++ server.py
@@ -60,19 +60,19 @@ def login():
                             tz=datetime.timezone.utc)
                     )
                     response = make_response(
-                        redirect(url_for('index', _external=True)))
+                        redirect(url_for('dashboard', _external=True)))
                     response.set_cookie('auth', token)
                     return response
                 else:
                     error = 'Password Incorrect'
 
         # If there's an error, render the index page with the error message
-        response = make_response(render_template('index.html', error=error))
+        response = make_response(render_template('/pages/index.html', error=error))
         response.headers['X-Content-Type-Options'] = 'nosniff'
         return response
 
     # For GET requests, render the index page with no error
-    response = make_response(render_template('index.html', error=''))
+    response = make_response(render_template('/pages/index.html', error=''))
     response.headers['X-Content-Type-Options'] = 'nosniff'
     return response
 
